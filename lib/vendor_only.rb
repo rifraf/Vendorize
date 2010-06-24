5ba2db36201a93eef99062c83baef00f32c63bac
rifraf <rifraf@rifraf.net>
2010-06-24 13:17:01 +0100

    Tweak to stop unnecessary loading of rubygems in vendor_only

--- lib/vendor_only.rb
+++ lib/vendor_only.rb
@@ -7,6 +7,9 @@ require bootfile if File.exists?(bootfile)
 $LOAD_PATH.delete_if {true} # Trick to clear a read-only array
 $LOAD_PATH.unshift(ENV['_Vendor_'] || './_vendor_')
 
+# Prevent rubygems from getting loaded
+$" << 'rubygems.rb'
+
 def vendor_only?
   true
 end
